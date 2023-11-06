f1bbc3a9714755c8752e57478e56dcec85ec71c3
James Miller <jam1211miller@gmail.com>
2023-11-06 10:48:49 -0700

    Finished Star Rating

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating />
+    <StarRating maxRating={5} />
   </React.StrictMode>
 );
